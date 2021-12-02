72c833a24553899f5f71b1a2be30ebd3def0b583
Deki97 <dejan97@libero.it>
2021-12-02 15:31:32 +0100

    h1 message done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
 // BONUS:
 // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 Vue.config.devtools = true;
 
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            titleMessage: 'Ciao sono un messaggio e vengo da VueJs!'
+        }
+    }
+)
